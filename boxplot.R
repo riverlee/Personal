045d4e05188415b03c4f26b1703396aa243be21f
Jiang Li <riverlee2008@gmail.com>
2013-07-20 13:13:53 -0700

    Update boxplot

--- boxplot.R
+++ boxplot.R
@@ -2,7 +2,6 @@
 # Boxplot for different size of data
 ########################################
 library(ggplot2)
-library(reshape2)
 
 s1<-rnorm(n=10,mean=2,sd=2)  # Normal distribution
 s2<-rbinom(n=50,size=10,prob=0.4)  # binomial distribution
@@ -13,4 +12,8 @@ dat<-data.frame(val=c(s1,s2,s3),dist=c(rep("Normal",length(s1)),
                                        rep("Poisson",length(s3))))
 
 # boxplot
+# using ggplot
 ggplot(data=dat,aes(x=dist,y=val))+geom_boxplot()
+
+# using boxplot 
+boxplot(val~dist,data=dat)
